70bc4deaddbb82537728e0c2052cb422080d1e6c
Yoandy M <yoandym@gmail.com>
2025-07-21 10:07:30 -0400

    [WIP] tool response schema

--- llm_tool/models/__init__.py
+++ llm_tool/models/__init__.py
@@ -2,4 +2,4 @@ from . import (llm_model, llm_provider, llm_tool, llm_tool_consent_config,
                llm_tool_greeting, llm_tool_model_inspector,
                llm_tool_model_method_executor, llm_tool_record_creator,
                llm_tool_record_retriever, llm_tool_record_unlinker,
-               llm_tool_record_updater, mail_message)
+               llm_tool_record_updater, llm_tool_response_schema, mail_message)
